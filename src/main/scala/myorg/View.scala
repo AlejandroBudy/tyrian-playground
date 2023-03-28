5c35069ed2779beb303fc24583ebc97e2153fe8b
Alejandro Torres Atochero <alejandro.torres@siriusxm.com>
2023-03-28 11:22:27 +0200

    navbar

--- src/main/scala/myorg/View.scala
+++ src/main/scala/myorg/View.scala
@@ -6,10 +6,42 @@ import tyrian.Html.`class`
 import tyrian.Tyrian
 
 def render(model: SpacesModel): Html[Msg] =
-  div(`class` := "container-fluid")(
-    nav(`class` := "navbar navbar-expand-lg bg-body-tertiary bg-primary")(
+  div(
+    nav(`class` := "navbar navbar-expand-lg bg-primary")(
       div(`class` := "container-fluid")(
-        span(`class` := "navbar-brand mb-0 h1")("Rocking Spaces")
+        a(`class` := "navbar-brand")("Rocking Spaces"),
+        button(
+          attributes(
+            ("class", "navbar-toggler"),
+            ("type", "button"),
+            ("data-bs-toggle", "collapse"),
+            ("data-bs-target", "#navbarNav"),
+            ("aria-controls", "navbarNav"),
+            ("aria-expanded", "false")
+          )
+        )(
+          span(`class` := "navbar-toggler-icon")()
+        ),
+        div(id := "navbarNav", `class` := "collapse navbar-collapse")(
+          ul(`class` := "navbar-nav me-auto mb-2 mb-lg-0")(
+            li(`class` := "nav-item")(
+              a(`class` := "nav-link")("spaces")
+            ),
+            li(`class` := "nav-item")(
+              a(`class` := "nav-link")("rooms")
+            )
+          ),
+          div(`class` := "d-flex")(
+            ul(`class` := "navbar-nav me-auto mb-2 mb-lg-0")(
+              li(`class` := "nav-item")(
+                a(`class` := "nav-link")("Login")
+              ),
+              li(`class` := "nav-item")(
+                a(`class` := "nav-link")("Signup")
+              )
+            )
+          )
+        )
       )
     ),
     div(
