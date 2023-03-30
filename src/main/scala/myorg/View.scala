c0a0f9411e305ad09ac8fc817726e15384ce8367
Alejandro Torres Atochero <alejandro.torres@siriusxm.com>
2023-03-30 21:05:54 +0200

    another try to navbar work, failure again

--- src/main/scala/myorg/View.scala
+++ src/main/scala/myorg/View.scala
@@ -7,43 +7,7 @@ import tyrian.Tyrian
 
 def render(model: SpacesModel): Html[Msg] =
   div(
-    nav(`class` := "navbar navbar-expand-lg bg-primary")(
-      div(`class` := "container-fluid")(
-        a(`class` := "navbar-brand")("Rocking Spaces"),
-        button(
-          attributes(
-            ("class", "navbar-toggler"),
-            ("type", "button"),
-            ("data-bs-toggle", "collapse"),
-            ("data-bs-target", "#navbarNav"),
-            ("aria-controls", "navbarNav"),
-            ("aria-expanded", "false")
-          )
-        )(
-          span(`class` := "navbar-toggler-icon")()
-        ),
-        div(id := "navbarNav", `class` := "collapse navbar-collapse")(
-          ul(`class` := "navbar-nav me-auto mb-2 mb-lg-0")(
-            li(`class` := "nav-item")(
-              a(`class` := "nav-link")("spaces")
-            ),
-            li(`class` := "nav-item")(
-              a(`class` := "nav-link")("rooms")
-            )
-          ),
-          div(`class` := "d-flex")(
-            ul(`class` := "navbar-nav me-auto mb-2 mb-lg-0")(
-              li(`class` := "nav-item")(
-                a(`class` := "nav-link")("Login")
-              ),
-              li(`class` := "nav-item")(
-                a(`class` := "nav-link")("Signup")
-              )
-            )
-          )
-        )
-      )
-    ),
+    renderNavbar,
     div(
       model.spaces.map { space =>
         div(
@@ -51,5 +15,42 @@ def render(model: SpacesModel): Html[Msg] =
           p(space.address)
         )
       }
+    )
+  )
+
+private def renderNavbar =
+  nav(`class` := "navbar navbar-expand-lg bg-primary")(
+    div(`class` := "container-fluid")(
+      a(`class` := "navbar-brand")("Rocking Spaces"),
+      button(
+        attributes(
+          ("class", "navbar-toggler"),
+          ("type", "button"),
+          ("data-bs-toggle", "collapse"),
+          ("data-bs-target", "#navbarNav")
+        )
+      )(
+        span(`class` := "navbar-toggler-icon")()
+      ),
+      div(id := "navbarNav", `class` := "collapse navbar-collapse")(
+        ul(`class` := "navbar-nav me-auto mb-2 mb-lg-0")(
+          li(`class` := "nav-item")(
+            a(`class` := "nav-link")("spaces")
+          ),
+          li(`class` := "nav-item")(
+            a(`class` := "nav-link")("rooms")
+          )
+        ),
+        div(`class` := "d-flex")(
+          ul(`class` := "navbar-nav me-auto mb-2 mb-lg-0")(
+            li(`class` := "nav-item")(
+              a(`class` := "nav-link")("Login")
+            ),
+            li(`class` := "nav-item")(
+              a(`class` := "nav-link")("Signup")
+            )
+          )
+        )
+      )
     )
   )
